8bd5575efea395be3826d3d8336b6e3cca5f6e38
Bartosz Kolek <bartoszkolek@yahoo.com>
2024-03-16 15:12:48 +0100

    working version v01

--- titanic/build.sbt
+++ titanic/build.sbt
@@ -6,3 +6,10 @@ lazy val root = (project in file("."))
   .settings(
     name := "titanic"
   )
+import sbt.Keys.libraryDependencies
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "3.4.0"
+
+libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.8"
--- titanic/src/main/scala/titanic.scala
+++ titanic/src/main/scala/titanic.scala
@@ -1,5 +1,40 @@
-object titanic extends App {
-  case class Passenger(id: String, name: String, survive: String)
-  class Passengers(Seq[Passenger])
+import com.github.tototoshi.csv.CSVReader
+import java.io.File
+import com.github.tototoshi.csv.defaultCSVFormat
+case class Passenger(id: Int, survive: Boolean, cabinClass: Int)
 
+object Titanic  extends App{
+  val file = new File("/home/bartosz/bigdata/Parser/parser/src/main/resources/titanic.csv")
+
+  def parse(file: File): List[Passenger] = {
+
+    def survive(binary: String): Boolean = {
+      (binary == "1")
+
+    }
+    try {
+      val reader = CSVReader.open(file)
+
+      // Pomiń nagłówki, jeśli istnieją
+      reader.readNext()
+      val collectionOfPassengers: List[Passenger] = reader
+        .toStream
+        .map(fields => Passenger(fields.head.toInt,
+          survive(fields(1)),
+          fields(2).toInt,
+        ))
+        .toList
+
+      reader.close()
+      collectionOfPassengers
+    } catch {
+      case e: Exception =>
+        println(s"Error occurred: ${e.getMessage}")
+        List.empty[Passenger]
+    }
+  }
+
+  // Wywołanie funkcji parse i przetworzenie pasażerów
+  val passengers = parse(file)
+  passengers.foreach(println)
 }
